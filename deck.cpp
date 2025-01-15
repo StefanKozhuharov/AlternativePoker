93798d50779ec470a285ab8e73271a1f85cf8b02
StefanKozhuharov <107200328+StefanKozhuharov@users.noreply.github.com>
2025-01-15 16:11:05 +0200

    Updated deck.cpp
    removed declared constants that is now in the constants header file and linked said file to this one

--- deck.cpp
+++ deck.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include "constants.h"
 using namespace std;
 
-int const timesToShuffle = 100;
 int cards[32] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 };
 
 int* shuffleDeck(int* deck) {
@@ -27,6 +27,6 @@ int getCardType(int card) {
 
 int getCardColour(int card) {
 
-	return card / 8;
+	return (card-1) / 8;
 
 }
