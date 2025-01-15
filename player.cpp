a23b46633ef4cc33d8aa82c4608e2ae9ef806a11
StefanKozhuharov <107200328+StefanKozhuharov@users.noreply.github.com>
2025-01-15 15:08:57 +0200

    Added the player structure and setPlayerCount function

--- player.cpp
+++ player.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
 #include "deck.h"
 using namespace std;
+
+int const CHIP_VALUE = 10;
+
+struct player {
+
+	int hand[3];
+	int balance = CHIP_VALUE * 100;
+	int current_bid;
+	int score;
+
+};
+
+int setPlayerCount() {
+
+	int totalPlayers = 1;
+
+	while (totalPlayers < 2 || totalPlayers > 9) {
+
+		cout << "How many players are going to play (2-9)?" << endl;
+		cin >> totalPlayers;
+
+	}
+
+	return totalPlayers;
+
+}
+
+int totalPlayers = setPlayerCount();
+player* players;
