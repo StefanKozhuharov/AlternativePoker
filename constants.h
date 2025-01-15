3b019fb8191c0c6e7646e35fde5d4b0da73a6470
StefanKozhuharov <107200328+StefanKozhuharov@users.noreply.github.com>
2025-01-15 16:09:40 +0200

    Added a header file for all constants

---
+++ constants.h
@@ -0,0 +1,19 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+extern const  int CHIP_VALUE = 10;
+extern const  int timesToShuffle = 100;
+extern const int SEVEN = 1; 
+extern const int EIGHT = 2; 
+extern const int NINE = 3; 
+extern const int TEN = 4; 
+extern const int JACK = 5;
+extern const int QUEEN = 6;
+extern const int KING = 7; 
+extern const int ACE = 0;
+extern const int CLUB = 0;
+extern const int DIAMOND = 1;
+extern const int HEART = 2;
+extern const int SPADE = 3;
+
+#endif
